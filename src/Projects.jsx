d18fad51295bea4b613aed7486b7bb0130b5d80e
OllisDev <ikermagro@outlook.es>
2026-09-09 18:10:30 +0200

    quit floor in Projects section

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,13 +1,13 @@
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Pipe from "./components/Pipe";
 
 function Projects() {
   return (
     <>
-      <Header />
-      <Pipe />
-      <Footer />
+      <div className="projects-page">
+        <Header />
+        <Pipe />
+      </div>
     </>
   );
 }
