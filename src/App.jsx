bc04fd8ec855530290953656f177b6e70d04d0ff
OllisDev <ikermagro@outlook.es>
2026-09-24 16:31:08 +0200

    refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./Index.jsx";
 import Projects from "./Projects.jsx";
 import AboutMe from "./AboutMe.jsx";
@@ -35,7 +35,7 @@ function UpdateMeta({ title, description }) {
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route
           path="/"
@@ -98,7 +98,7 @@ function App() {
           }
         />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
